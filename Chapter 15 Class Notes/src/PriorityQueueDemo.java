1ddfaf70d3bf1b850a125233df9aad55dfb0f90a
Derek J Miller <DJMiller@Naperville203.org>
2023-10-03 13:26:30 -0500

    Stack & Queue Demos Complete

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,23 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        // Create a priority queue of to-do items
+        // A WorkOrder has a message ID that is used to determine priority
+        // A priority queue can only store Comparable objects
+        Queue<WorkOrder> toDo = new PriorityQueue<>();
+
+        toDo.add(new WorkOrder(3, "Water Plants"));
+        toDo.add(new WorkOrder(2, "Make Dinner"));
+        toDo.add(new WorkOrder(2,   "Walk Dog"));
+        toDo.add(new WorkOrder(9, "Play Videogames"));
+        toDo.add(new WorkOrder(1, "Study for the Chapter 15 Exam"));
+
+        // Objects added to a priority queue are NOT stored in priority order
+        System.out.println(toDo);
+
+        while (toDo.size() > 0) {
+            System.out.println(toDo.remove());
+        }
+
     }
 }
--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,26 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        // Create a print queue of strings (using a linked link)
+        Queue<String> jobs = new LinkedList<>();
+
+        // Add some print jobs
+        jobs.add("Joe: Expense Report 2023");
+        jobs.add("Cathy: Top Secret Document #5");
+
+        System.out.println("Printing: "+jobs.remove());
+
+        jobs.add("Cathy: Really Top Secret Document #2");
+        jobs.add("Joe: Grocery List");
+        jobs.add("Cathy: Can I Get Fired For This?");
+
+        System.out.println("Printing: "+jobs.remove());
+
+        jobs.add("Boss: Cathy's Termination Letter");
+
+        // Print the rest of the jobs in the queue
+        while (jobs.size() > 0) {
+            System.out.println("Printing: "+jobs.remove());
+        }
     }
 }
