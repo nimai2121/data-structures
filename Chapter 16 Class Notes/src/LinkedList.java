6aae1338d4710bd88c8f04ed964f53984b640c0f
Derek J Miller <DJMiller@Naperville203.org>
2023-10-17 13:29:19 -0500

    Finished LinkedList implementation.

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -71,6 +71,9 @@ public class LinkedList
         Returns an iterator for iterating through this list.
         @return an iterator for iterating through this list
     */
+    public ListIterator listIterator() {
+        return new LinkedListIterator();
+    }
 
 
 
@@ -108,6 +111,10 @@ public class LinkedList
             @return the traversed element
         */
         public Object next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
             previous = position;
             isAfterNext = true;
 
@@ -130,6 +137,14 @@ public class LinkedList
             Tests if there is an element after the iterator position.
             @return true if there is an element after the iterator position
         */
+        public boolean hasNext() {
+            // Return false if there are no elements in the list
+            if (position == null) {
+                return first != null;
+            }
+
+            return position.next != null;
+        }
 
 
         /**
@@ -137,7 +152,22 @@ public class LinkedList
             and moves the iterator past the inserted element.
             @param element the element to add
         */
+        public void add(Object element) {
+            // Check if the iterator is at the start of the list
+            if (position == null) {
+                addFirst(element);
+                position = first;
+            }
+            else {
+                Node newNode = new Node();
+                newNode.data = element;
+                newNode.next = position.next;
+                position.next = newNode;
+                position = newNode;
+            }
 
+            isAfterNext = false;
+        }
 
 
 
@@ -147,6 +177,22 @@ public class LinkedList
             Removes the last traversed element. This method may
             only be called after a call to the next() method.
         */
+        public void remove() {
+            if (!isAfterNext) {
+                throw new IllegalStateException();
+            }
+
+            if (position == first) {
+                removeFirst();
+                position = null;
+            }
+            else {
+                previous.next = position.next;
+                position = previous;
+            }
+
+            isAfterNext = false;
+        }
 
 
 
@@ -158,6 +204,13 @@ public class LinkedList
             Sets the last traversed element to a different value.
             @param element the element to set
         */
+        public void set(Object element) {
+            if (!isAfterNext) {
+                throw new IllegalStateException();
+            }
+
+            position.data = element;
+        }
 
 
 
--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -5,5 +5,16 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        LinkedList students = new LinkedList();
+        students.addFirst("Preston");
+        students.addFirst("Emma");
+        students.addFirst("William");
+        students.addFirst("Addison");
+
+        ListIterator iterator = students.listIterator();
+        iterator.next();
+        iterator.add("Hyder");
+        iterator.next();
+        iterator.remove();
     }
 }
