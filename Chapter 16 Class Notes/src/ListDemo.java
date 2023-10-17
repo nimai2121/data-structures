39ef4c374bd182f95cf548375eb14c292fa6dc3d
Derek J Miller <DJMiller@Naperville203.org>
2023-10-17 14:20:53 -0500

    Finished LinkedList implementation.

--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -5,5 +5,12 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        LinkedList students = new LinkedList();
+        students.addFirst("Esha");
+        students.addFirst("Seth");
+        students.addFirst("Celeste");
+        students.addFirst("Andrew");
+
+        System.out.println(students);
     }
 }
