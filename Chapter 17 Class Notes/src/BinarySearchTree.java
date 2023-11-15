3d26884878d31b7d59b1c51a6b0c99e33d607062
Derek J Miller <DJMiller@Naperville203.org>
2023-11-15 14:21:15 -0600

    Finished BST through Cases 1 & 2 of Removal

--- Chapter 17 Class Notes/src/BinarySearchTree.java
+++ Chapter 17 Class Notes/src/BinarySearchTree.java
@@ -12,7 +12,7 @@ public class BinarySearchTree
     */
     public BinarySearchTree()
     {   
-        
+        this.root = null;
     }
     
     /**
@@ -21,7 +21,17 @@ public class BinarySearchTree
     */
     public void add(Comparable obj) 
     {   
-        
+        Node newNode = new Node();
+        newNode.data = obj;
+        newNode.left = null;
+        newNode.right = null;
+
+        if (this.root == null) {
+            this.root = newNode;
+        }
+        else {
+            this.root.addNode(newNode);
+        }
     }
 
     /**
@@ -31,6 +41,20 @@ public class BinarySearchTree
     */
     public boolean find(Comparable obj)
     {
+        Node current = this.root;
+
+        while (current != null) {
+            int diff = obj.compareTo(current.data);
+            if (diff == 0) {
+                return true;
+            }
+            else if (diff < 0) {
+                current = current.left;
+            }
+            else {
+                current = current.right;
+            }
+        }
         return false;
     }
     
@@ -41,7 +65,51 @@ public class BinarySearchTree
     */
     public void remove(Comparable obj)
     {
-        
+        Node toBeRemoved = this.root;
+        Node parent = null;
+        boolean found = false;
+
+        while (!found && toBeRemoved != null) {
+            int diff = obj.compareTo(toBeRemoved.data);
+            if (diff == 0) {
+                found = true;
+            }
+            else {
+                parent = toBeRemoved;
+                if (diff < 0) {
+                    toBeRemoved = toBeRemoved.left;
+                }
+                else {
+                    toBeRemoved = toBeRemoved.right;
+                }
+            }
+        }
+
+        if (!found) {
+            return;
+        }
+
+        // Case 1 and Case 2 (At least one child is null)
+        if (toBeRemoved.left == null || toBeRemoved.right == null) {
+            Node newChild;
+
+            if (toBeRemoved.left == null) {
+                newChild = toBeRemoved.right;
+            }
+            else {
+                newChild = toBeRemoved.left;
+            }
+
+            if (parent == null) {
+                this.root = newChild;
+            }
+            else if (parent.left == toBeRemoved) {
+                parent.left = newChild;
+            }
+            else {
+                parent.right = newChild;
+            }
+        }
     }
     
     /**
@@ -67,7 +135,10 @@ public class BinarySearchTree
     */
     static class Node
     {   
-        
+        // A BST MUST BE made up of Comparable Objects
+        public Comparable data;
+        public Node left;
+        public Node right;
 
         /**
             Inserts a new node as a descendant of this node.
@@ -75,7 +146,27 @@ public class BinarySearchTree
         */
         public void addNode(Node newNode)
         {   
-            
+            // If diff < 0, newNode is to the left of this node
+            // If diff > 0, newNode is to the right of this node
+            int diff = newNode.data.compareTo(data);
+
+            if (diff < 0) {
+                if (left == null) {
+                    left = newNode;
+                }
+                else {
+                    left.addNode(newNode);
+                }
+            }
+            else if (diff > 0) {
+                if (right == null) {
+                    right = newNode;
+                }
+                else {
+                    right.addNode(newNode);
+                }
+            }
+
         }
     }
 }
