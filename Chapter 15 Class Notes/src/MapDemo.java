6b852c4dbf2a8744cf4cc98b86d3551b80b3f808
Dr. Derek Miller <83288861+MillerNCHS@users.noreply.github.com>
2024-09-10 14:21:10 -0500

    MapDemo Complete
    Finished Map lesson.

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,29 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        /*
+         * The Map interface is generic.
+         * The first type given is the type of the key.
+         * The second type given is the type of the value.
+         */
+        Map<String, Color> favColors = new HashMap<>();
+
+        // Add elements to the map using the put method
+        favColors.put("Henry", Color.BLUE);
+        favColors.put("Jack", Color.RED);
+        favColors.put("Priya", Color.GREEN);
+
+        // Two different elements with the same value
+        favColors.put("Dr. Miller", Color.GREEN);
+
+        // The same key CANNOT have two different values
+        // Using put on a key that exists, changes the value
+        favColors.put("Jack", Color.MAGENTA);
+
+        // Create a set of the keys in a map
+        Set<String> keys = favColors.keySet();
+        for (String key: keys) {
+            System.out.println(key + " (" + key.hashCode() + "): "+favColors.get(key));
+        }
     }
 }
