fca5f2d4aeb7752aed367bb73ad99dfb34d3d519
nimai2121 <nrnagireddy@stu.naperville203.org>
2025-08-25 13:16:39 -0500

    1st day

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,30 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
-        
+        /* the addLast method can be used ot populate a list. */
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Tony");
+        staff.addLast("Natasha");
+        staff.addLast("Peter");
+        staff.addLast("Steve");
+
+        System.out.println(staff);
+
+        // the list is currently: TNPS
+        /* 
+         * The listIterator method creates a new list iterator
+         * that is positioned at the head of the list. 
+         * The | is used to represent the iterator position. 
+        */
+
+        ListIterator<String> iterator = staff.listIterator(); // |TNPS
+
+        /* The next method advances the iterator OVER the next element in the list. 
+        */
+        iterator.next(); // T|NPS
+
+        /* The next method also returns the element that the iterator passes over. */
+        String avenger = iterator.next();
+        System.out.println(avenger); // Should print Natasha 
     }
 }
