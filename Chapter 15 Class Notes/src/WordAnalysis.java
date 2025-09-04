4d48a3cbe6bbf3e3682c054ebe4566b98091828a
nimai2121 <nrnagireddy@stu.naperville203.org>
2025-09-04 13:28:48 -0500

    d

--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -13,6 +13,36 @@ public class WordAnalysis
     public static void main(String[] args)
         throws FileNotFoundException
     {
+        // Determine the current working directory 
+        // System.out.println(System.getProperty("user.dir"));
+
+        // read the dictionary file and the novel file.
+        Set<String> dictionaryWords = readWords("Chapter 15 Class Notes/src/words");
+        Set<String> novelWords = readWords("Chapter 15 Class Notes/src/war-and-peace.txt");
+
+        // Print all the words that are in the novel, but not in the dictionary
+        //int i = 0;
+
+        for (String word: novelWords) {
+            if(!dictionaryWords.contains(word)) {
+                System.out.println(word);
+                //i++;
+            }
+        }
+
+        // Print out the number of unique words in the novel
+        System.out.println("There are " + novelWords.size()+" unique words.");
+        //System.out.println("There are " + i + " unique words.");
+
+        // Print the number of unique words with > 3 letters
+        Iterator<String> iterator = novelWords.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().length() <= 3) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println("There are " + novelWords.size() + " unique > 3 letter words.");
     }
 
     /**
@@ -23,8 +53,23 @@ public class WordAnalysis
      * word is a sequence of upper- and lowercase letters.
     */
     public static Set<String> readWords(String filename)
+        
         throws FileNotFoundException
     {
-        return null;
+        //we use a HashSet instead of a TreeSet because the order doesn't matter. 
+        Set<String> words = new HashSet<>();
+
+        Scanner in = new Scanner(new File(filename), "UTF-8");
+
+        // USE any character that's not a letter as a delimiter.
+        in.useDelimiter("[^a-zA-Z]+");
+
+        while (in.hasNext()) {
+            // Add words to the set (duplicates are automatically ignored)
+            words.add(in.next().toLowerCase());
+
+        }
+
+        return words;
     }
 }
