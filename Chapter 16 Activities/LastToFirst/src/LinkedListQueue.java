3259b3ab94a21f99878012c785a325b572ef2473
nimai2121 <nrnagireddy@stu.naperville203.org>
2025-10-02 13:24:48 -0500

    l2f redo

--- Chapter 16 Activities/LastToFirst/src/LinkedListQueue.java
+++ Chapter 16 Activities/LastToFirst/src/LinkedListQueue.java
@@ -26,16 +26,23 @@ public class LinkedListQueue
     {
         //. . .
 
-        for (int i = 0; i < 5; i++) {
+        /*for (int i = 0; i < 5; i++) {
             Object n = this.remove();
             add(n);
+        }*/
+
+        if (head != null && head != tail) { //not already having the head at the tail.
+            Node current = head; //current head its on
+            // Traverse to the node just before the tail
+            while (current.next != tail) {
+                current = current.next;
+            }
+            Node oldTail = tail; // store old tail
+            tail = current; // update tail to the node before the old tail
+            tail.next = null; // set new tail's next to null
+            oldTail.next = head; // link old tail to the front of the list
+            head = oldTail; // update head to be the old tail
         }
-
-        
-
-
-
-
 
     }
 
